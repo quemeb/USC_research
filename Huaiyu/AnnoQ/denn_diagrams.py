2775c26113b2f79cc9a3509610a0679993121fa1
Bryan Queme <bryanmqueme@gmail.com>
2024-02-01 14:36:59 -0800

    Updated

---
+++ Huaiyu/AnnoQ/denn_diagrams.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 25 20:51:25 2024
+
+@author: quemeb
+"""
+
+import pandas as pd
+from matplotlib_venn import venn2, venn3
+import matplotlib.pyplot as plt
+
+
+ df = pd.read_csv("/Users/queme/Desktop/USC_research/Huaiyu/AnnoQ/inter_results_new.csv")
